b3d744bc611aa18f8ff8a3cebe364303cc47c041
Zhongyuan Jin <nanomalloc@gmail.com>
2020-03-29 13:53:06 +0300

    try named export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export * as flatten from './flatten'
-export * as iterate from './iterate'
-export * as generate from './generate'
+// export * as flatten from './flatten'
+// export * as iterate from './iterate'
+// export * as generate from './generate'
+
+import flatten from './flatten'
+import iterate from './iterate'
+import generate from './generate'
+export { flatten, iterate, generate }
